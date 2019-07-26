f266ec71589d10d822699804f626eb9006ce8236
Phikiso, Obakeng <obakeng.phikiso@accenture.com>
2019-07-26 13:08:04 +0200

    router

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -36,7 +36,7 @@ loginUser: any = {};
         color: 'success'
       });
       toast.present();
-      this.router.navigate(['tabs/profile'])
+      this.router.navigate(['/profile']);
     }).catch(async () => {
       const toast = await this.toastController.create({
         message: 'Incorrect password or email!.',
