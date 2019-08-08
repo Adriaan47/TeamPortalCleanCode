d43dcd22392adaef33e6ddf442de0bdc3e6eb42a
Pienaar, Adriaan H <adriaan.h.pienaar@accenture.com>
2019-08-08 09:40:52 +0200

    Logout page working

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -159,17 +159,22 @@ export class UsersService {
   // Email link to reset password
   resetPassword(email: string) {
     return this.afAuth.auth.sendPasswordResetEmail(email).then(() => {
-      this.presentAlert('Password reset', 'Password reset email sent, check your inbox.') 
+      this.presentAlert('Password reset', 'Password reset email sent, check your inbox.')
     }).catch(error => this.presentAlert('Error occured ', error.message));
-    }
-    async presentAlert(title: string, content: string){
-      const alert = await this.alertController.create({
-        header: title,
-        message: content,
-        buttons: ['OK']
+  }
+  async presentAlert(title: string, content: string) {
+    const alert = await this.alertController.create({
+      header: title,
+      message: content,
+      buttons: ['OK']
 
-      });
-      await alert.present();
-    }
+    });
+    await alert.present();
   }
 
+  logout() {
+    return this.afAuth.auth.signOut().then(() => {
+      this.router.navigate(['/login']);
+    });
+  }
+}
