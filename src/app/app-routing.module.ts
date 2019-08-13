79aa368b58850d650dbf6fcfc91d8581d2ddb42d
Pienaar, Adriaan H <adriaan.h.pienaar@accenture.com>
2019-08-13 11:36:36 +0200

    Fixed img cropper route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './guard/auth.guard';
+import { ComponentsComponent } from './profile/components/components.component';
 
 
 const routes: Routes = [
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule', canActivate: [AuthGuard] },
+  { path: 'upload-picture', component: ComponentsComponent },
   { path: '**', loadChildren: './login/login.module#LoginPageModule' },
 ];
 @NgModule({
