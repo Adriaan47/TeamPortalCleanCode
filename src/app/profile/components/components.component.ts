2ba941933e91c13e2a7a560f2ff8f3f9cdbf56ca
Pienaar, Adriaan H <adriaan.h.pienaar@accenture.com>
2019-08-16 11:29:03 +0200

    Fixed edit picture route

--- src/app/profile/components/components.component.ts
+++ src/app/profile/components/components.component.ts
@@ -61,7 +61,7 @@ export class ComponentsComponent implements OnInit {
           .update({
             profilePicture: imgage,
           }).then(() => {
-            this.router.navigate(['edit-profile']);
+            this.router.navigate(['/tabs/profile/edit-profile']);
           }).catch(err => {
             return err.message;
           });
